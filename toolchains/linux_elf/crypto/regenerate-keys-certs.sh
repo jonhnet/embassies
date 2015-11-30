578b362f34228765a8c229a0f05dcf5ca38b7343
jonh <jonh.embassies@jonh.net>
2015-11-29 20:35:30 -0800

    create root keys automatically

--- toolchains/linux_elf/crypto/regenerate-keys-certs.sh
+++ toolchains/linux_elf/crypto/regenerate-keys-certs.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+if [ ! -e keys/root.keys ]; then
+	echo "No root keys defined. I don't want to create new ones automatically,"
+	echo "to avoid a broken script stomping your keys and certs."
+	echo "If this is a fresh Embassies environment and you want certs created, "
+	echo "please type 'yes'"
+	read answer
+	if [ X$answer == X'yes' ]; then
+		echo "Generating keys and certs..."
+	else
+		echo "Quitting without keys present."
+		exit 1
+	fi
+else
+	echo Keys already present
+	exit 0
+fi
+
 NAMES="com gnucash.org graphics.net maps.net marble.maps.net net office.org org raster.graphics.net results.search.com retailer.com search.com spreadsheet.office.org vector.graphics.net vendor-a vendor-b widgets.retailer.com wordprocessor.office.org zftp-zoog craigslist ebay microsoft reddit craigslist-warm ebay-warm microsoft-warm reddit-warm cpuid-test"
 
 # Rebuild the root
